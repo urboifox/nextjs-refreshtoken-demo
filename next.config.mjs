d76ebb15f5deaddeb8fb5700c998cf303cab308a
Mohamed Ashraf <96639620+urboifox@users.noreply.github.com>
2024-08-23 14:40:00 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -10,7 +10,7 @@ const nextConfig = {
                 // 1- you can use axios interceptor, which is against what we are trying to do
                 // 2- make an endpoint as a proxy for each 3rd party api endpoint, which is boring
                 // 3- or simply, add these 2 lines  here
-                // these 2 lines add a proxy automatically to all requests coming to your server (if theres no api route for them)
+                // these 2 lines add a proxy automatically to all requests going to your server (if theres no api route for them)
                 // for example: if you have a route at /api/profile it will requests this endpoint
                 // but if you don't, then it will redirect it to http://localhosr:3001/profile
                 // AND THE GREAT THING IS, they reach the middleware.ts file which is all we want.
