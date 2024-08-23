6f42fa4e69721280fa123fa6e240ee50f4a53c35
Mohamed Ashraf <96639620+urboifox@users.noreply.github.com>
2024-08-23 20:45:52 +0300

    Update page.tsx

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,14 +1,15 @@
 import { cookies } from "next/headers";
 import Link from "next/link";
+import { LOCAL_URL } from "@/app/constants"
 
 // example request that needs an access token
 export default async function ProfilePage() {
     // even though this request is for an internal route, but theres no /api/profile in our app
     // so the request will be redirected to API_ENDPOINT/profile (more info at next.config.mjs)
     // - if you get a type error: URL is invalid
-    // you can add "http://localhost:3000" at the start of the url, or add it to a variable
-    // ex: export const LOCAL_URL = process.env.NODE_ENV === "development" ? "http://localhost:3000" : "https://production.com";
-    const res = await fetch("/api/profile", {
+    // make sure you add the absolute url, relative paths are only available in client side, because it's relative to the document.
+    // but theres no document in the server.
+    const res = await fetch(LOCAL_URL + "/api/profile", {
         headers: {
             // you should send the cookies manually with the requests that are sent from server side, so that the cookies reaches the middleware.ts.
             // put in mind that this makes this route dynamic, meaning that next will pre-render it on each request.
